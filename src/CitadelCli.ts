74d4f8912faa4210200ed5c70afa8bef74207196
James Childers <james.childers@gmail.com>
2025-02-18 13:23:29 -0500

    more styling

--- src/CitadelCli.ts
+++ src/CitadelCli.ts
@@ -1,29 +1,9 @@
-import { html, css, LitElement, unsafeCSS } from 'lit';
+import { html, css, LitElement } from 'lit';
 import { globalStyles } from './styles/global-styles.js';
 import { property } from 'lit/decorators.js';
 
 export class CitadelCli extends LitElement {
-  static styles = [
-    globalStyles,
-    css`
-    :host {
-      /* Apply global styles directly to host */
-      ${globalStyles}
-
-      display: block;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 40vh;
-      background-color: var(--citadel-bg, rgb(17, 24, 39));
-      color: var(--citadel-cli-text-color, #0f0);
-      color: #0f0;
-      box-sizing: border-box;
-      margin: 0;
-      padding: 0;
-    }
-  `];
+  static styles = globalStyles;
 
   @property({ type: String }) header = 'Hey there';
 
--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,7 +1,28 @@
 import { css } from 'lit';
 
 export const globalStyles = css`
-  /* Global CSS Custom Properties */
-  --citadel-bg: rgb(17, 24, 39);
-  --citadel-cli-text-color: #0f0;
+  :host {
+    --citadel-bg: rgb(17, 24, 39);
+    --citadel-text: rgba(255, 255, 255, 0.87);
+
+    --citadel-border: rgb(55, 65, 81);
+    --citadel-accent: #646cff;
+    --citadel-accent-hover: #535bf2;
+    --citadel-min-height: 200px;
+    --citadel-max-height: 80vh;
+    --citadel-default-height: 35vh;
+    --citadel-error: rgb(239, 68, 68);
+
+    background-color: var(--citadel-bg, rgb(17, 24, 39));
+    color: var(--citadel-text, rgb(0, 99, 30));
+    display: block;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40vh;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 `;
