1e9ef194a223b2e7d9a23c9d75b665ab039dda01
James Childers <james.childers@gmail.com>
2025-02-26 12:50:53 -0500

    Get wtr working with single .ts tests

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,10 +1,12 @@
 // import { playwrightLauncher } from '@web/test-runner-playwright';
 
+// Needed for wtr to work with TypeScript
+import { esbuildPlugin } from "@web/dev-server-esbuild";
+
 const filteredLogs = ['Running in dev mode', 'Lit is in dev mode'];
 
 export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
-  /** Test files to run */
-  files: 'dist/**/*.test.js',
+  files: 'dist/**/*.test.{js,ts}',
 
   /** Resolve bare module imports */
   nodeResolve: {
@@ -24,6 +26,8 @@ export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
     }
     return true;
   },
+
+  plugins: [esbuildPlugin({ ts: true })], // Enable TypeScript support
 
   /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
   // esbuildTarget: 'auto',
